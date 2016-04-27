c29392368548944ed494bee9de49ef4dc86d78ac
bradhamson <brad.hamson@gmail.com>
2016-04-27 13:33:46 -0400

    inherit leaflet positions

--- dist/L.Control.Rose.js
+++ dist/L.Control.Rose.js
@@ -1,26 +1,35 @@
 L.Control.Rose = L.Control.extend({
-
+	includes: L.Mixin.Events,
+    
     options: {
         icon: 'default',
         iSize: 'small',
         position: 'bottomleft',
     },
 
+    placeholder: '',
+
     initialize: function (placeholder, options) {
         L.setOptions(this, options);
+        this.placeholder = placeholder;
+        console.log(this.placeholder);
+       },
+
+    onAdd: function (map) {
+    	this._map = map;
 
         //Find content container
-        var content = this._contentContainer = L.DomUtil.get(placeholder);
+        var content = this._contentContainer = L.DomUtil.get(this.placeholder);
+        console.log(content);
 
         // Remove the content container from its original parent
         content.parentNode.removeChild(content);
 
         //Create rose container
         var container = this._container =
-            L.DomUtil.create('div', 'leaflet-rose ' + this.options.position);
+            L.DomUtil.create('div', 'leaflet-rose');
 
-        //Style and attach content container
-        L.DomUtil.addClass(content, 'leaflet-control');
+        //attach content container
         container.appendChild(content)
 
         //insert image
@@ -29,33 +38,9 @@ L.Control.Rose = L.Control.extend({
         var path = 'images/'+arrow+'.svg';
         var i = L.DomUtil.create('img', s, content);
         i.setAttribute('src', path);
-    },
+        console.log(container);
 
-    addTo: function (map) {
-        var container = this._container;
-        var content = this._contentContainer;
-
-        //attach rose container to controls container
-        var controlContainer = map._controlContainer;
-        controlContainer.insertBefore(container, controlContainer.firstChild);
-
-        this._map = map;
-
-        return this;
-    },
-
-    removeFrom: function (map) {
-        var container = this._container;
-        var content = this._contentContainer;
-
-        // remove rose container from controls container
-        var controlContainer = map._controlContainer;
-        controlContainer.removeChild(container);
-
-        // disassociate the map object
-        this._map = null;
-
-        return this;
+        return container;
     }
 
 });
